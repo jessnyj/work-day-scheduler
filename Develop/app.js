b67fbe32ae636d41896b427067ebaf2e58e28055
jessny <jessnyjoseph@gmail.com>
2021-01-04 21:40:18 -0800

    added save to local storage

--- Develop/app.js
+++ Develop/app.js
@@ -3,14 +3,29 @@ $(document).ready(function () {
     $("#currentDay").text(moment().format("dddd, MMMM Do")); 
 })
 
-
-// Time blocks for business hours
-
-
 // Time block is color coded for past, present, and future
 
-// click time block, can enter event
+function time () {
+    var currentTime = moment.hour();
+
+    $(".time-block").each(function(){
+    
+    })
+}
+
+
 
 // click save button to save event in local storage
+$("#9am .description").val(localStorage.getItem("9am"));
+$("#10am .description").val(localStorage.getItem("10am"));
+$("#11am .description").val(localStorage.getItem("11am"));
+$("#12pm .description").val(localStorage.getItem("12pm"));
+$("#1pm .description").val(localStorage.getItem("1pm"));
+$("#2pm .description").val(localStorage.getItem("2pm"));
+$("#3pm .description").val(localStorage.getItem("3pm"));
+$("#4pm .description").val(localStorage.getItem("4pm"));
+$("#5pm .description").val(localStorage.getItem("5pm"));
+
 
 // when refresh page, saved events persist
+
