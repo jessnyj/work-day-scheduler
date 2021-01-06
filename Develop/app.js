e1ab48f31a5a8a3ebd290bdb8e2426273bea7f21
jessny <jessnyjoseph@gmail.com>
2021-01-05 16:10:14 -0800

    made index.html moved to root of the folder

--- Develop/app.js
+++ Develop/app.js
@@ -5,27 +5,35 @@ $(document).ready(function () {
 
 // Time block is color coded for past, present, and future
 
-function time () {
-    var currentTime = moment.hour();
+// function time () {
+//     var currentTime = moment.hour();
 
-    $(".time-block").each(function(){
-    
-    })
-}
+//     $("#time-block").each(function(){
+//         var block = parseInt(($this).prop("id"));
+        
+//         if (block > currentTime && black < currentTime + 6) {
+//             $(this).addClass()
+//         }
+// }
 
 
 
 // click save button to save event in local storage
-$("#9am .description").val(localStorage.getItem("9am"));
-$("#10am .description").val(localStorage.getItem("10am"));
-$("#11am .description").val(localStorage.getItem("11am"));
-$("#12pm .description").val(localStorage.getItem("12pm"));
-$("#1pm .description").val(localStorage.getItem("1pm"));
-$("#2pm .description").val(localStorage.getItem("2pm"));
-$("#3pm .description").val(localStorage.getItem("3pm"));
-$("#4pm .description").val(localStorage.getItem("4pm"));
-$("#5pm .description").val(localStorage.getItem("5pm"));
+$("9am").val(localStorage.setItem("description", "value"));
+
+
+$("#10am .description").val(localStorage.setItem("10am"));
+$("#11am .description").val(localStorage.setItem("11am"));
+$("#12pm .description").val(localStorage.setItem("12pm"));
+$("#1pm .description").val(localStorage.setItem("1pm"));
+$("#2pm .description").val(localStorage.setItem("2pm"));
+$("#3pm .description").val(localStorage.setItem("3pm"));
+$("#4pm .description").val(localStorage.setItem("4pm"));
+$("#5pm .description").val(localStorage.setItem("5pm"));
 
 
 // when refresh page, saved events persist
+// $("#saveBtn").on("click", function() {
+//     var 
+// })
 
