999ec2f3e6eaa5fd226f925ad962a9f8675613f0
jessny <jessnyjoseph@gmail.com>
2021-01-05 22:17:51 -0800

    completion of readme

--- Develop/script.js
+++ Develop/script.js
@@ -1,13 +1,13 @@
 // Current Day
 $(document).ready(function () {
-    $("#currentDay").text(moment().format("dddd, MMMM Do")); 
+    $("#currentDay").text(moment().format("dddd, MMMM Do"));
 })
 
 // Color Change
-function time () {
+function time() {
     var currentTime = moment().format('H');
-    
-    $(".time-block").each(function(){
+
+    $(".time-block").each(function () {
         var scheduleTime = parseInt($(this).attr("id"));
 
         if (scheduleTime === currentTime) {
@@ -40,12 +40,12 @@ $(".saveBtn").on("click", function () {
 })
 
 // click save button to save event in local storage
-    $("#9am .description").val(localStorage.getItem("9am"));
-    $("#10am .description").val(localStorage.getItem("10am"));
-    $("#11am .description").val(localStorage.getItem("11am"));
-    $("#12pm .description").val(localStorage.getItem("12pm"));
-    $("#1pm .description").val(localStorage.getItem("1pm"));
-    $("#2pm .description").val(localStorage.getItem("2pm"));
-    $("#3pm .description").val(localStorage.getItem("3pm"));
-    $("#4pm .description").val(localStorage.getItem("4pm"));
-    $("#5pm .description").val(localStorage.getItem("5pm"));
+$("#9am .description").val(localStorage.getItem("9am"));
+$("#10am .description").val(localStorage.getItem("10am"));
+$("#11am .description").val(localStorage.getItem("11am"));
+$("#12pm .description").val(localStorage.getItem("12pm"));
+$("#1pm .description").val(localStorage.getItem("1pm"));
+$("#2pm .description").val(localStorage.getItem("2pm"));
+$("#3pm .description").val(localStorage.getItem("3pm"));
+$("#4pm .description").val(localStorage.getItem("4pm"));
+$("#5pm .description").val(localStorage.getItem("5pm"));
--- README.md
+++ README.md
@@ -11,20 +11,49 @@
 * jQuery
 
 ## Summary
+This calendar application allows a user to view what the current day is, and has time blocks
+for business hours that allows a user to enter and save events. These time blocks are also
+color coded in order to display whether it is the past, present or future. 
 
 
 ## Deployed Link
 
-* [See Live Site]
-
+* [See Live Site] (https://jessnyj.github.io/work-day-scheduler/)
 
 
 ## Work Involved
+For this application, I utilized Moment.js in order to add the date within the schedule
+and the hour in order to track the color coded time blocks. I also created the timeblocks utilizing 
+jQuery. Furthermore, I added the ability to save events to the page through local storage. 
 
 
 ## Code Snippet
+```
+function time () {
+    var currentTime = moment().format('H');
+    
+    $(".time-block").each(function(){
+        var scheduleTime = parseInt($(this).attr("id"));
 
+        if (scheduleTime === currentTime) {
+            $(this).addClass("present");
+            $(this).removeClass("past");
+            $(this).removeClass("future");
+        } else if (scheduleTime < currentTime) {
+            $(this).removeClass("present");
+            $(this).addClass("past")
+            $(this).removeClass("future");
+        } else {
+            $(this).removeClass("past");
+            $(this).addClass("future")
+            $(this).removeClass("present");
+        }
+    });
 
+}
+
+time();
+```
 
 
 ## Authors
