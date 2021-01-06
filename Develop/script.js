f6653235e853c53d9841b29d90263b90e8a7c890
jessny <jessnyjoseph@gmail.com>
2021-01-05 20:19:49 -0800

    added timeblocks with jQuery

--- Develop/script.js
+++ Develop/script.js
@@ -5,35 +5,42 @@ $(document).ready(function () {
 
 // Time block is color coded for past, present, and future
 
-// function time () {
-//     var currentTime = moment.hour();
+function time () {
+    var currentTime = moment().format('H');
+    console.log(currentTime)
+    var timeBlocks = ["9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm"]
+    var timeDiv = $("#timeOfDay");
 
-//     $("#time-block").each(function(){
-//         var block = parseInt(($this).prop("id"));
-        
-//         if (block > currentTime && black < currentTime + 6) {
-//             $(this).addClass()
-//         }
-// }
+    
+    // make for loop iterating through timeBlocks
+    for (var i = 0; i < timeBlocks.length; i++) {
+        var newTimeBlock = $("<div class='row time-block'><div class='col-md-1 hour'>" + timeBlocks[i] + "</div><textarea class='col-md-10 description'></textarea> <button class='col-md-1 saveBtn'><i class='fas fa-save'></i></button></div>" )
+        timeDiv.append(newTimeBlock);
+    }
+    // new div for each timeblock
+    // append to timeOfDay
+}
+
+time();
 
 
-
-// click save button to save event in local storage
-$("9am").val(localStorage.setItem("description", "value"));
-
-
-$("#10am .description").val(localStorage.setItem("10am"));
-$("#11am .description").val(localStorage.setItem("11am"));
-$("#12pm .description").val(localStorage.setItem("12pm"));
-$("#1pm .description").val(localStorage.setItem("1pm"));
-$("#2pm .description").val(localStorage.setItem("2pm"));
-$("#3pm .description").val(localStorage.setItem("3pm"));
-$("#4pm .description").val(localStorage.setItem("4pm"));
-$("#5pm .description").val(localStorage.setItem("5pm"));
 
 
 // when refresh page, saved events persist
-// $("#saveBtn").on("click", function() {
-//     var 
-// })
+$(".saveBtn").on("click", function () {
+    var hour = $(this).parent().attr("id");
+    var text = $(this).siblings(".description").val();
 
+    localStorage.setItem(hour, text);
+})
+
+// click save button to save event in local storage
+    $("#9am .description").val(localStorage.getItem("9am"));
+    $("#10am .description").val(localStorage.getItem("10am"));
+    $("#11am .description").val(localStorage.getItem("11am"));
+    $("#12pm .description").val(localStorage.getItem("12pm"));
+    $("#1pm .description").val(localStorage.getItem("1pm"));
+    $("#2pm .description").val(localStorage.getItem("2pm"));
+    $("#3pm .description").val(localStorage.getItem("3pm"));
+    $("#4pm .description").val(localStorage.getItem("4pm"));
+    $("#5pm .description").val(localStorage.getItem("5pm"));
